0513be4ff0937ec77b3309bfbe47e6779b7f83b5
alaabaraka <>
2021-05-05 12:24:29 +0000

    practingjava

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+// comments are notes about the code you can create them by doing ctrl+/ or /* mulitble line of comments */
+
+// one line of comments
+
+/*
+mulitble lines of comments
+*/
+
+ 
+
+alert('Hello ');
+
+var bookshops= prompt('what is last book name you  raed? litreture  or  human resourses or  scince ');
+
+
+if(bookshops=='litreture'){
+  document.write('<h1> great job');
+}else if( bookshops==='human resourses'){
+  document.write('excited!  *--*');
+}else if(bokshops === 'since'){
+  document.write('<h2>excelent');}
+else{
+  document.write('img src"https://i0.wp.com/mingmandarin.com/wp-content/uploads/2020/12/Lets-learn-how-to-say-Excuse-Me-in-Chinese.jpg?w=440&ssl=1"' );
+}
+
